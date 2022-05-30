33d2503ab9749883e08a7f4745569dfead3e2c8d
shoval-ba <shoval-ba@cyber4s.dev>
2022-05-30 15:52:49 +0300

    add localstroge

--- compied/todo.js
+++ compied/todo.js
@@ -1,26 +1,20 @@
 "use strict";
-let tasks = [];
 var imgTrash;
-let ul;
-class Task {
-    // deleted :boolean;
-    // done : any;
-    constructor(value) {
-        this.value = value;
-        // this.deleted =false;
-        // this.done = false;
+// let ul:HTMLElement;
+let taskArray = [];
+function getInputValue() {
+    // let inputValue:string = document.body.getElementsByClassName("input-text").value;
+    const input = document.getElementById('input-text');
+    if (input != null) {
+        let value = input.value;
+        return value;
     }
-    getInputValue() {
-        // let inputValue:string = document.body.getElementsByClassName("input-text").value;
-        const input = document.getElementById('input-text');
-        if (input != null) {
-            this.value = input.value;
-            return this.value;
-        }
-    }
-    createB() {
-        localStorage.setItem("task", this.value);
-        // Create just if there task
+}
+function createBox() {
+    let inputValue = getInputValue();
+    if (document.body.getElementsByTagName("input")[0].value !== "") {
+        let ul = document.getElementsByTagName("ul")[0];
+        // Create the box
         let box = document.createElement('li');
         ul.appendChild(box);
         box.id = "box";
@@ -29,7 +23,7 @@ class Task {
         imgTrash.src = "filled-trash.png";
         imgTrash.classList.add('trash');
         box.appendChild(imgTrash);
-        imgTrash.addEventListener('click', this.deleteBox);
+        imgTrash.addEventListener('click', deleteBox);
         // Create the icon of task
         var img = document.createElement('img');
         img.src = "task.png";
@@ -40,48 +34,68 @@ class Task {
         checkbox.type = 'checkbox';
         checkbox.id = 'checkbox';
         box.appendChild(checkbox);
+        // Add the input value to the box
         let task = document.createElement('p');
-        task.innerText += localStorage.task;
+        task.innerText += inputValue;
         box.appendChild(task);
         task.classList.add("task");
-    }
-    deleteBox() {
-        var div = imgTrash.parentElement;
-        if (div !== null) {
-            div.remove();
-        }
-    }
-    checkDone() {
-        const check_done = document.getElementById('checkbox');
-        if (check_done?.checked === true)
-            console.log("check");
-    }
-}
-function createTask() {
-    var inputValue = Task.prototype.getInputValue();
-    if (document.body.getElementsByTagName("input")[0].value !== "") {
-        let new_task = new Task(inputValue);
-        new_task.createB();
-        // Clean the input to the next  task
+        taskArray.push(inputValue);
+        localStorage.setItem("tasks", taskArray.toString());
         document.body.getElementsByTagName("input")[0].value = "";
     }
 }
-function afterClick() {
-    createTask();
+// Delete box when click on the trash icon
+function deleteBox(e) {
+    let currentRemove = e.target;
+    // let removeList = currentRemove.parentElement;
+    currentRemove.parentElement.remove();
+}
+function checkDone() {
+    const check_done = document.getElementById('checkbox');
+    if (check_done?.checked === true)
+        console.log("check");
 }
 function create() {
-    // Creates a title.
-    let heading = document.createElement('H1');
-    heading.innerText += "TODO:";
-    document.body.appendChild(heading);
-    heading.classList.add("h1");
-    let button = document.createElement('button');
-    button.innerText += "+";
-    document.body.appendChild(button);
-    button.classList.add("button-1");
-    ul = document.createElement('ul');
-    document.body.appendChild(ul);
-    button.addEventListener('click', createTask);
+    let ul = document.getElementsByTagName("ul")[0];
+    let tasks = localStorage.getItem("tasks");
+    const taskArray = tasks?.split(",");
+    console.log(taskArray);
+    for (let i = 0; i < taskArray.length; i++) {
+        console.log(taskArray[i]);
+        let box = document.createElement('li');
+        console.dir(ul);
+        box.id = "box";
+        // Create the icon of delete
+        imgTrash = document.createElement('img');
+        imgTrash.src = "filled-trash.png";
+        imgTrash.classList.add('trash');
+        box.appendChild(imgTrash);
+        imgTrash.addEventListener('click', deleteBox);
+        // Create the icon of task
+        var img = document.createElement('img');
+        img.src = "task.png";
+        img.classList.add('task-icon');
+        box.appendChild(img);
+        // Create checkbox
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = 'checkbox';
+        box.appendChild(checkbox);
+        let task1 = document.createElement('p');
+        task1.innerText += taskArray[i];
+        box.appendChild(task1);
+        task1.classList.add("task");
+        ul.appendChild(box);
+    }
+    let button = document.querySelector(".button-1");
+    let background = document.querySelector(".background");
+    if (background !== null) {
+        ul = document.createElement('ul');
+        background.appendChild(ul);
+    }
+    if (button !== null) {
+        button.addEventListener('click', createBox);
+    }
 }
 // Call the function who creates the app
 window.onload = () => {
--- todo.ts
+++ todo.ts
@@ -1,32 +1,27 @@
 "use strict";
-let tasks:any[]=[];
-var imgTrash:HTMLImageElement;
-let ul:HTMLElement;
-class Task {
-    value:any;
-    // deleted :boolean;
-    // done : any;
-    constructor(value:any ){
-        this.value = value;
-        // this.deleted =false;
-        // this.done = false;
-        
-    }
 
-    getInputValue() {
+var imgTrash:HTMLImageElement;
+// let ul:HTMLElement;
+let taskArray:any[]=[];
+
+function getInputValue() {
         // let inputValue:string = document.body.getElementsByClassName("input-text").value;
         const input = document.getElementById('input-text') as HTMLInputElement;
         if (input != null) {
-            this.value = input.value;
-            return this.value;
+            let value = input.value;
+            return value;
         }
     }
 
-    createB(){
+function createBox(){
 
-        localStorage.setItem("task" , this.value);
+    let inputValue = getInputValue()!;
+    
+    if(document.body.getElementsByTagName("input")[0].value !==""){
 
-        // Create just if there task
+        let ul = (document.getElementsByTagName("ul")[0] as HTMLUListElement);
+
+        // Create the box
         let box= document.createElement('li');
         ul.appendChild(box);
         box.id ="box";  
@@ -37,7 +32,7 @@ class Task {
         imgTrash.classList.add('trash');
         box.appendChild(imgTrash);
 
-        imgTrash.addEventListener('click' , this.deleteBox);
+        imgTrash.addEventListener('click' , deleteBox);
 
         // Create the icon of task
         var img = document.createElement('img');
@@ -51,65 +46,83 @@ class Task {
         checkbox.id = 'checkbox';
         box.appendChild(checkbox);
 
+        // Add the input value to the box
         let task:HTMLElement = document.createElement('p');
-        task.innerText += localStorage.task;
+        task.innerText += inputValue;
         box.appendChild(task);
         task.classList.add("task");
-    }
 
-    deleteBox(){
-        var div = imgTrash.parentElement; 
-        if(div !== null){
-         div.remove();
-        }
-    }
-
-    checkDone(){
-        const check_done = document.getElementById('checkbox') as HTMLInputElement | null;
-        if(check_done?.checked === true) 
-            console.log("check")
-    }
-     
-}
-
-function createTask(){
-    var inputValue = Task.prototype.getInputValue();
-    if(document.body.getElementsByTagName("input")[0].value !==""){
-        let new_task = new Task(inputValue);
-        new_task.createB();
-         // Clean the input to the next  task
+        taskArray.push(inputValue);
+        
+        localStorage.setItem("tasks", taskArray.toString());
+        
         document.body.getElementsByTagName("input")[0].value = "";
     }
 }
 
-
-function afterClick(){
-    createTask()
+// Delete box when click on the trash icon
+function deleteBox(e:any){
+    let currentRemove = e.target;
+    // let removeList = currentRemove.parentElement;
+    currentRemove.parentElement.remove();
 }
+
+function checkDone(){
+    const check_done = document.getElementById('checkbox') as HTMLInputElement | null;
+    if(check_done?.checked === true) 
+        console.log("check")
+}   
 
 
 function create(){
+    let ul = (document.getElementsByTagName("ul")[0] as HTMLUListElement);
+    let tasks = localStorage.getItem("tasks");
+    const taskArray = tasks?.split(",")
+    console.log(taskArray);
+    for(let i =0; i<taskArray!.length; i++){
+        console.log(taskArray![i]);
+        let box= document.createElement('li');
+        console.dir(ul)
+        box.id ="box";
+         // Create the icon of delete
+         imgTrash = document.createElement('img');
+         imgTrash.src = "filled-trash.png";
+         imgTrash.classList.add('trash');
+         box.appendChild(imgTrash);
+         imgTrash.addEventListener('click' , deleteBox);
+         // Create the icon of task
+         var img = document.createElement('img');
+         img.src = "task.png";
+         img.classList.add('task-icon');
+         box.appendChild(img);
+         // Create checkbox
+         var checkbox = document.createElement('input');
+         checkbox.type = 'checkbox';
+         checkbox.id = 'checkbox';
+         box.appendChild(checkbox);
+        let task1:HTMLElement = document.createElement('p');
+        task1.innerText += taskArray![i];
+        box.appendChild(task1);
+        task1.classList.add("task");
+        ul.appendChild(box);
+    }
 
-    // Creates a title.
-    let heading:HTMLElement = document.createElement('H1');
-    heading.innerText += "TODO:";
-    document.body.appendChild(heading);
-    heading.classList.add("h1");
-
-    let button:HTMLElement = document.createElement('button');
-    button.innerText += "+";
-    document.body.appendChild(button);
-    button.classList.add("button-1");
-
-    ul = document.createElement('ul');
-    document.body.appendChild(ul);
-
-    button.addEventListener('click' , createTask);
+    let button = document.querySelector(".button-1");
+    let background = document.querySelector(".background");
+    if(background !==null){
+        ul = document.createElement('ul');
+        background.appendChild(ul);
+    }
     
-
+    if(button !== null){
+        button.addEventListener('click' , createBox);
+    }
+    
 }
 
 // Call the function who creates the app
 window.onload =() => {
+    
+
     create()
 }
